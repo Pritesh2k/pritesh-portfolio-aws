0ed28891b0c724ec7fe6e52f81dafe4d7aa2b988
Pritesh2k <priteshparekh237@gmail.com>
2023-12-28 17:30:02 +0000

    Update to skills section

--- src/JS/Skills.js
+++ src/JS/Skills.js
@@ -1,79 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../CSS/skills.css';
 
 import TerminalIcon from '@mui/icons-material/Terminal';
 import SchoolIcon from '@mui/icons-material/School';
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 
+import TerminalVideo from '../Assets/terminal.mp4';
+import GraduationVideo from '../Assets/graduation.mp4';
+import TrophyVideo from '../Assets/trophy.mp4';
+
+const skillsData = [
+    {
+        icon: <TerminalIcon fontSize='large' />,
+        video: TerminalVideo,
+        categories: [
+            'Python', 'JavaScript', 'Java', 'React.js',
+            'C#', 'R', 'HTML & CSS', 'Node.js'
+        ]
+    },
+    {
+        icon: <SchoolIcon fontSize='large' />,
+        video: GraduationVideo,
+        categories: [
+            'Artificial Intelligence', 'Data Analytics', 'Database Management', 'Unit Testing',
+            'Cybersecurity', 'Data Structures', 'Object Orientated Programming'
+        ]
+    },
+    {
+        icon: <EmojiEventsIcon fontSize='large' />,
+        video: TrophyVideo,
+        categories: [
+            'NPM', 'Rest APIs', 'Project Management', 'MS Office',
+            'Git', 'Project Architecture, Problem Solver, Team Player'
+        ]
+    }
+];
+
 function Skills() {
+    const [expandedCards, setExpandedCards] = useState(Array(skillsData.length).fill(false));
+
+    const handleCardClick = (index) => {
+        const newExpandedCards = [...expandedCards];
+        newExpandedCards[index] = !newExpandedCards[index];
+        setExpandedCards(newExpandedCards);
+    };
+
     return (
-        <div className="container">
-            <div>
-                <div className="content">
-                    <div className='icon-1'>
-                        <TerminalIcon fontSize='large' />
-                    </div>
-                    <div className='content-context'>
-                        <ul>
-                            <li>Python</li>
-                            <li>JavaScript</li>
-                            <li>Java</li>
-                            <li>React.js</li>
+        <>
+            <div className='skills-container'>
+                {skillsData.map((skill, index) => (
+                    <div
+                        key={index}
+                        className={`skill-card ${expandedCards[index] ? 'expanded' : ''}`}
+                        onClick={() => handleCardClick(index)}
+                    >
+                        <div className={`skill-icon skill-icon-${index + 1}`}>
+                            {skill.icon}
+                        </div>
+                        <ul className='skill-list'>
+                            {skill.categories.map((category, i) => (
+                                <li key={i}>{category}</li>
+                            ))}
                         </ul>
-
-                        <ul>
-                            <li>C#</li>
-                            <li>R</li>
-                            <li>HTML & CSS</li>
-                            <li>Node.js</li>
-                        </ul>
+                        <video autoPlay muted loop className='video-player'>
+                            <source src={skill.video} type='video/mp4' />
+                            Your browser does not support the video tag.
+                        </video>
                     </div>
-                </div>
+                ))}
             </div>
-            <div>
-                <div className="content">
-                    <div className='icon-2'>
-                        <SchoolIcon fontSize='large' />
-                    </div>
-                    <div className='content-context'>
-                        <ul>
-                            <li>Artificial Intelligence</li>
-                            <li>Data Analytics</li>
-                            <li>Database Management</li>
-                            <li>Unit Testing</li>
-                        </ul>
-
-                        <ul>
-                            <li>Cybersecurity</li>
-                            <li>Data Structures</li>
-                            <li>Object Orientated Programming</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <div className="content">
-                    <div className='icon-3'>
-                        <EmojiEventsIcon fontSize='large' />
-                    </div>
-                    <div className='content-context'>
-                        <ul>
-                            <li>NPM</li>
-                            <li>Rest APIs</li>
-                            <li>Project Management</li>
-                            <li>MS Office</li>
-                        </ul>
-
-                        <ul>
-                            <li>Git</li>
-                            <li>Project Architecture</li>
-                            <li>Problem Solver</li>
-                            <li>Team Player</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </>
     );
 }
 
