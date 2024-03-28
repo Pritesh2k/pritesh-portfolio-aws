32a22bfe86a63af56ded63fdb602f8f0234ce6a5
Pritesh2k <priteshparekh237@gmail.com>
2024-03-28 14:32:52 +0000

    project 3/3

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -18,51 +18,115 @@ function Projects() {
       </div>
       <div className='projects-subheader'>
         <div id='scroll-box' className='project-nav'>
-          <a onClick={() => handleProjectClick('FYP')}>Timley Crime Information System</a>
+          <a onClick={() => handleProjectClick('FYP')}>Timely Crime Information System</a>
           <a onClick={() => handleProjectClick('web-portfolio')}>Web Portfolio</a>
-          <a onClick={() => handleProjectClick('python-scripting')}>Python Client Server Script</a>
+          <a onClick={() => handleProjectClick('python-scripting')}>Python Client Server</a>
         </div>
 
-        {/* Apply 'active' class when selectedProject matches the ID */}
-        <div id='FYP' className={`projects-scroll-container ${selectedProject === 'FYP' ? 'active' : ''}`}>
-          <div className='project-description'>
-            <div className='project-title'>
-              Timley Crime Information System
-            </div>
-            <div className='project-link'>
-              <GitHubIcon fontSize='large'/>
-            </div>
+        {/* Render project content based on selectedProject */}
+        <div className='projects-scroll-container'>
+          {selectedProject === 'FYP' && (
+            <div id='FYP' className='project-content'>
+              <div className='project-description'>
+                <div className='project-title'>
+                  Timley Crime Information System
+                </div>
+                <div className='project-link'>
+                  <GitHubIcon fontSize='large' />
+                </div>
 
-            <div className='project-context'>
-              <div className='methods-header'>Methods & Technuiqes</div>
-              <ul className='project-list'>
-                <li>Agile - Feature Driven Methodology</li>
-                <li>System Design</li>
-                <ul>
-                  <li>Flow Charts</li>
-                  <li>Wire Frames</li>
-                  <li>UX Principles</li>
-                </ul>
-                <li>Java Object Orientated Programming</li>
-                <li>Crime Prediction Analysis - Random Forest + R Packages, Python</li>
-                <li>Maps & News API</li>
-                <li>Database Management</li>
-              </ul>
+                <div className='project-context'>
+                  <div className='methods-header'>Methods & Technuiqes</div>
+                  <ul className='project-list'>
+                    <li>Agile - Feature Driven Methodology</li>
+                    <li>System Design</li>
+                    <ul>
+                      <li>Flow Charts</li>
+                      <li>Wire Frames</li>
+                      <li>UX Principles</li>
+                    </ul>
+                    <li>Java Object Orientated Programming</li>
+                    <li>Crime Prediction Analysis - Random Forest + R Packages, Python</li>
+                    <li>Maps & News API</li>
+                    <li>Database Management</li>
+                  </ul>
 
-              <div className='methods-header'>Features</div>
-              <ul className='project-list'>
-                <li>Crime Map - Google API</li>
-                <li>Locality based News Feed - News API + Location (Google Services)</li>
-                <li>Emergency button - automatically dials 999</li>
-                <li>Safety alerts - Alerts user when they enter 5km within recorded crime</li>
-                <li>Prediction Analysis - determins the likley hood as percentage of future crime with coonfindence level</li>
-              </ul>
+                  <div className='methods-header'>Features</div>
+                  <ul className='project-list'>
+                    <li>Crime Map - Google API</li>
+                    <li>Locality based News Feed - News API + Location (Google Services)</li>
+                    <li>Emergency button - automatically dials 999</li>
+                    <li>Safety alerts - Alerts user when they enter 5km within recorded crime</li>
+                    <li>Prediction Analysis - determins the likley hood as percentage of future crime with coonfindence level</li>
+                  </ul>
+                </div>
+              </div>
             </div>
-            
-          </div>
+          )}
+
+          {selectedProject === 'web-portfolio' && (
+            <div id='web-portfolio' className='project-content'>
+              <div className='project-description'>
+                <div className='project-title'>
+                  web-portfolio
+                </div>
+                <div className='project-link'>
+                  <GitHubIcon fontSize='large' />
+                </div>
+
+                <div className='project-context'>
+                  <div className='methods-header'>Methods & Technuiqes</div>
+                  <ul className='project-list'>
+                    <li>AWS Cloud</li>
+                    <li>Continuous Integration / Continuous Deployment</li>
+                    <li>Git - Version Control</li>
+                    <li>React Framework</li>
+                    <li>JavaScript & CSS</li>
+                    <li>System Design</li>
+                    <li>UX Principles</li>
+                  </ul>
+
+                  <div className='methods-header'>Features</div>
+                  <ul className='project-list'>
+                    <li>Personal Presentation of aquired skills and worked on projects</li>
+                    <li>Responsiveness</li>
+                    <li>Site Theme</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {selectedProject === 'python-scripting' && (
+            <div id='python-scripting' className='project-content'>
+              <div className='project-description'>
+                <div className='project-title'>
+                  Python Client Server
+                </div>
+                <div className='project-link'>
+                  <GitHubIcon fontSize='large' />
+                </div>
+
+                <div className='project-context'>
+                  <div className='methods-header'>Methods & Technuiqes</div>
+                  <ul className='project-list'>
+                    <li>Python Script</li>
+                    <li>Python Client & Server Sockets</li>
+                    <li>TCP / IPv4</li>
+                    <li>WireShark</li>
+                  </ul>
+
+                  <div className='methods-header'>Features</div>
+                  <ul className='project-list'>
+                    <li>Continuous Communication between Client and Server until user chooses to exit</li>
+                    <li>Local port packet monitoring via WireShark</li>
+                  </ul>
+                </div>
+
+              </div>
+            </div>
+          )}
         </div>
-
-        {/* Repeat for other projects */}
       </div>
     </div>
   );
